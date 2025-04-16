6b7c826b9cc2cf2514958d194cc4fbb966e3f6ae
kevinlingfeng <kevinlingfengli@outlook.com>
2025-04-16 19:10:36 +0100

    updated notes

---
+++ docs/mathjax-config.js
@@ -0,0 +1,27 @@
+MathJax = {
+    tex: {
+      macros: {
+        E: "\\mathbb{E}",
+        V: "\\mathbb{V}",
+        P: "\\mathbb{P}",
+        eps: "\\varepsilon",
+        pr: "\\varphi",
+        b: "\\boldsymbol",
+        ind: "\\perp\\!\\!\\!\\!\\perp",
+        pt: "{\\textcolor{purple}{Y_t^{(1)}}}",
+        pc: "{\\textcolor{purple}{Y_t^{(0)}}}",
+        blue: "\\textcolor{blue}", 
+        red: "\\textcolor{red}",
+        purple: "\\textcolor{purple}",
+        set: "\\mathcal"
+      }
+    },
+    chtml: {
+        scale: 1.15,
+        mtextInheritFont: true,
+        mtextFont: 'Cambria',
+    },
+    startup: {
+        typeset: true
+    }
+  };
